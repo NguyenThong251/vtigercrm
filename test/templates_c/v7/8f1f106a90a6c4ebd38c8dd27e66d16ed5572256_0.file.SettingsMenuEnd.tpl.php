dd26d5fda2630f859dcd03dc50d42ed0737fe4e6
Nguyễn Hoàng Thông <125713226+NguyenThong251@users.noreply.github.com>
2026-05-10 13:26:16 +0700

    add db

---
+++ test/templates_c/v7/8f1f106a90a6c4ebd38c8dd27e66d16ed5572256_0.file.SettingsMenuEnd.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 4.5.5, created on 2026-05-10 06:24:08
+  from 'C:\xampp\htdocs\vtigercrm\layouts\v7\modules\Settings\Vtiger\SettingsMenuEnd.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.5',
+  'unifunc' => 'content_6a00248874a3e6_32756438',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8f1f106a90a6c4ebd38c8dd27e66d16ed5572256' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\vtigercrm\\layouts\\v7\\modules\\Settings\\Vtiger\\SettingsMenuEnd.tpl',
+      1 => 1752039682,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6a00248874a3e6_32756438 (Smarty_Internal_Template $_smarty_tpl) {
+?></div></div><?php }
+}
